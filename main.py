3251f1becf118502387379937586841a134e305c
VZYKIN <92172085+VZYKIN@users.noreply.github.com>
2021-11-18 04:35:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+def mergelists(obj1111, obj2222):
+    if obj1111.getDate() < obj2222.getDate():
+        return -1
+    elif obj1111.getDate() > obj2222.getDate():
+        return 1
+    else:
+        return 0
+
+
+
+list = vzlist1 + vzlist2
+list.sort(compareDate)
