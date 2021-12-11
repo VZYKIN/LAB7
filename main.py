6baa7c1140b40a2d11637e0a8fe92abaa3ce6d8a
VZYKIN <92172085+VZYKIN@users.noreply.github.com>
2021-12-11 18:32:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+# Лабораторная работа 7
+def merge(*lists):
+    new_line = [] # новый список
+    temporary_list = []
 
-def mergelists(obj1111, obj2222):
-    if obj1111.getDate() < obj2222.getDate():
-        return -1
-    elif obj1111.getDate() > obj2222.getDate():
-        return 1
-    else:
-        return 0
+    for i in range(len(lists[0]) - 1, -1, -1):  # объединяем
+        temporary_list = temporary_list + lists[0][i]
 
-
-
-list = vzlist1 + vzlist2
-list.sort(compareDate)
+    while temporary_list:
+        sort_num = min(temporary_list) # ищем мин значения в списке
+        while sort_num in temporary_list: # добавляем значение
+            new_line.append(sort_num)
+            temporary_list.remove(sort_num)
+            
+    return new_line
